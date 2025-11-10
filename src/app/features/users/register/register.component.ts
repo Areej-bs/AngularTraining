698f7b6a10001139bed2d30f777765bf3b8e7495
Erij Ben Salah <124561146+Areej-bs@users.noreply.github.com>
2025-11-10 11:41:44 +0100

    form validation

--- src/app/features/users/register/register.component.ts
+++ src/app/features/users/register/register.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {User} from '../../../models/user';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/user';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,31 +10,45 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
   user: User;
   formRegister: FormGroup;
-  constructor() {
-  }
+
   ngOnInit() {
     this.user = new User();
     this.formRegister = new FormGroup({
       firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
       lastName: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      email: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-
-    })
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/)
+      ]),
+      address: new FormGroup({
+        street: new FormControl('', Validators.required),
+        city: new FormControl('', Validators.required),
+        state: new FormControl('', Validators.required),
+        zip: new FormControl('', Validators.required)
+      }),
+      phones: new FormArray([new FormControl('', Validators.required)])
+    });
   }
-  save(){
-    this.user=this.formRegister.getRawValue();
+
+  get phones() {
+    return this.formRegister.get('phones') as FormArray;
+  }
+
+  addPhone() {
+    this.phones.push(new FormControl('', Validators.required));
+  }
+
+  removePhone(index: number) {
+    this.phones.removeAt(index);
+  }
+
+  save() {
+    this.user = this.formRegister.getRawValue();
     console.log(this.user);
-    //service to persist the object => save
-    //
-
+    // Call service to persist user here
   }
-  //todo 0=> pattern for email => a..z"@"a..z"."a..z & password => 8 char => aA1@
-  // todo 1=> work on address: {
-  //     street: string;
-  //     city: string;
-  //     state: string;
-  //     zip: string;
-  //   }
-  //todo 2=> work on the list of phone numbers => dynamic list +1 number
 }
